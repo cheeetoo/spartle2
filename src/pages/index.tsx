cdc48fb45c8d152d3cf3b4ac03c2a29215797ed1
Finn Cairns <fjamescairns@gmail.com>
2023-09-26 22:55:50 -0500

    header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
       <Script src="/script.js"></Script>
+      <header className="text-white flex justify-center text-4xl font-bold border-b-2 w-full overflow-hidden p-4 mb-4 border-[#39393c] font-sans">Spartle</header>
       <main>
         <div className="alert-container" data-alert-container></div>
         <div data-guess-grid className="guess-grid">
