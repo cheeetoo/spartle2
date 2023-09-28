88e3b39d03c8c8386c87b77ffae5c4cac71960c5
Finn Cairns <fjamescairns@gmail.com>
2023-09-28 07:19:59 -0500

    update admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,36 +1,49 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { createClient } from '@vercel/kv';
-// import { client } from '@/lib/kv_client';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { createClient } from "@vercel/kv";
 
 export default function Admin() {
   const router = useRouter();
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
   const client = createClient({
-    url: process.env.NEXT_PUBLIC_KV_REST_API_URL ?? '',
-    token: process.env.NEXT_PUBLIC_KV_REST_API_TOKEN ?? '',
+    url: process.env.NEXT_PUBLIC_KV_REST_API_URL ?? "",
+    token: process.env.NEXT_PUBLIC_KV_REST_API_TOKEN ?? "",
   }); // TODO: unsafe af, fix
 
   useEffect(() => {
-    const password = prompt('Please enter the admin password:');
+    const password = prompt("Please enter the admin password:");
 
     if (password !== process.env.NEXT_PUBLIC_ADMIN_PASSWD) {
-      alert('Incorrect password');
-      router.push('/');
+      alert("Incorrect password");
+      router.push("/");
     }
   }, []);
 
-    const updateWord = async (word: string) => {
-        console.log(word);
-        await client.set('word', word);
+  const updateWord = async (word: string) => {
+    if (word.length !== 5) {
+      return alert("Word must be 5 letters long");
     }
+    await client.set("word", word);
+  };
 
   return (
-    <div className='flex'>
-      <h1 className='text-white m-2'>Change word</h1>
-      <input type="text" value={word} onChange={(e) => setWord(e.target.value)} className='m-2'/>
-      <button onClick={async () => {await updateWord(word)}} className='bg-white border border-gray-500 m-2'>Submit</button>
+    <div className="flex">
+      <h1 className="text-white m-2">Change word</h1>
+      <input
+        type="text"
+        value={word}
+        onChange={(e) => setWord(e.target.value)}
+        className="m-2"
+      />
+      <button
+        onClick={async () => {
+          await updateWord(word);
+        }}
+        className="bg-white border border-gray-500 m-2"
+      >
+        Submit
+      </button>
     </div>
   );
 }
