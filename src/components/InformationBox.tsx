565b887766dc98b204ea826af3eba9997ba26cdd
Finn Cairns <fjamescairns@gmail.com>
2023-09-28 11:23:26 -0500

    close button

--- src/components/InformationBox.tsx
+++ src/components/InformationBox.tsx
@@ -28,6 +28,28 @@ export default function InformationBox() {
       style={customStyles}
       overlayClassName="bg-transparent"
     >
+      <div className="flex flex-col h-full font-sans">
+        <div className="flex justify-end p-4">
+          <button
+            onClick={() => setIsOpen(false)}
+            className="focus:outline-none"
+          >
+            <svg
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              height="20"
+              viewBox="0 0 24 24"
+              width="20"
+              className="h-5 w-5"
+            >
+              <path
+                fill="#ffffff"
+                d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
     </Modal>
   );
 }
