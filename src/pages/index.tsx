a8dd48e9ea602c2cb34d1136ebae0be82cf86651
Finn Cairns <fjamescairns@gmail.com>
2023-12-08 17:05:45 -0600

    Revert "remove information modal"
    This reverts commit ffe731cf0bf24b6d8817ac45231051cb7ae5c7b0.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const guessGridRef = useRef<HTMLDivElement>(null);
   const alertContainerRef = useRef<HTMLDivElement>(null);
 
-  // const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
   const [dictionary, setDictionary] = useState<string[]>([]);
   const [targetWord, setTargetWord] = useState<string>("");
   const [dataFetched, setDataFetched] = useState(false);
@@ -50,10 +50,10 @@ export default function Home() {
 
   return (
     <div className="flex flex-col min-h-screen m-0 bg-[#121213] [font-size:_clamp(0.5rem,2.5vmin,1.5rem)]">
-      <header className="text-white flex justify-center items-center border-b-2 w-full overflow-hidden p-4 mb-4 border-[#39393c]">
-        {/* <div></div> */}
+      <header className="text-white flex justify-between items-center border-b-2 w-full overflow-hidden p-4 mb-4 border-[#39393c]">
+        <div></div>
         <div className="sm:text-4xl font-bold font-sans text-3xl">Spartle</div>
-        {/* <button onClick={() => setIsOpen(true)}>
+        <button onClick={() => setIsOpen(true)}>
           <svg
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
@@ -66,10 +66,10 @@ export default function Home() {
               d="M14.8333 23H17.1666V20.6667H14.8333V23ZM15.9999 4.33334C9.55992 4.33334 4.33325 9.56001 4.33325 16C4.33325 22.44 9.55992 27.6667 15.9999 27.6667C22.4399 27.6667 27.6666 22.44 27.6666 16C27.6666 9.56001 22.4399 4.33334 15.9999 4.33334ZM15.9999 25.3333C10.8549 25.3333 6.66659 21.145 6.66659 16C6.66659 10.855 10.8549 6.66668 15.9999 6.66668C21.1449 6.66668 25.3333 10.855 25.3333 16C25.3333 21.145 21.1449 25.3333 15.9999 25.3333ZM15.9999 9.00001C13.4216 9.00001 11.3333 11.0883 11.3333 13.6667H13.6666C13.6666 12.3833 14.7166 11.3333 15.9999 11.3333C17.2833 11.3333 18.3333 12.3833 18.3333 13.6667C18.3333 16 14.8333 15.7083 14.8333 19.5H17.1666C17.1666 16.875 20.6666 16.5833 20.6666 13.6667C20.6666 11.0883 18.5783 9.00001 15.9999 9.00001Z"
             ></path>
           </svg>
-        </button> */}
+        </button>
       </header>
       <main>
-        {/*<InformationBox modalIsOpen={modalIsOpen} setIsOpen={setIsOpen} />*/}
+        <InformationBox modalIsOpen={modalIsOpen} setIsOpen={setIsOpen} />
         <div className="alert-container" ref={alertContainerRef}></div>
         <div className="guess-grid" ref={guessGridRef}>
           {Array.from({ length: 30 }).map((_, index) => (
