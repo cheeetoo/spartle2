14cbb71591749e78e189072fe09b83bcb17b7bd4
Finn Cairns <fjamescairns@gmail.com>
2023-12-10 02:33:43 -0600

    usable on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,51 +68,54 @@ export default function Home() {
           </svg>
         </button>
       </header>
-      <main>
+      {/* <main className="flex justify-center items-center flex-grow flex-col"> */}
+      <main className="">
         <InformationBox modalIsOpen={modalIsOpen} setIsOpen={setIsOpen} />
         <div className="alert-container" ref={alertContainerRef}></div>
-        <div className="guess-grid" ref={guessGridRef}>
-          {Array.from({ length: 30 }).map((_, index) => (
-            <div className="tile" key={index}></div>
-          ))}
-        </div>
-        <div
-          className="grid grid-cols-[repeat(20,_minmax(auto,_1.25em))] auto-rows-[3em] gap-[0.25em] justify-center"
-          ref={keyboardRef}
-        >
-          {"QWERTYUIOP".split("").map((letter) => (
-            <button className="key" data-key={letter} key={letter}>
-              {letter}
+        <div className="flex flex-col justify-center items-center">
+          <div className="guess-grid" ref={guessGridRef}>
+            {Array.from({ length: 30 }).map((_, index) => (
+              <div className="tile" key={index}></div>
+            ))}
+          </div>
+          <div
+            className="grid sm:grid-cols-[repeat(20,_minmax(auto,_1.25em))] sm:auto-rows-[3em] grid-cols-[repeat(20,_minmax(auto,_1.7em))] auto-rows-[3.8em] gap-[0.25em] justify-center"
+            ref={keyboardRef}
+          >
+            {"QWERTYUIOP".split("").map((letter) => (
+              <button className="key" data-key={letter} key={letter}>
+                {letter}
+              </button>
+            ))}
+            <div className="space"></div>
+            {"ASDFGHJKL".split("").map((letter) => (
+              <button className="key" data-key={letter} key={letter}>
+                {letter}
+              </button>
+            ))}
+            <div className="space"></div>
+            <button data-enter className="key large">
+              Enter
             </button>
-          ))}
-          <div className="space"></div>
-          {"ASDFGHJKL".split("").map((letter) => (
-            <button className="key" data-key={letter} key={letter}>
-              {letter}
+            {"ZXCVBNM".split("").map((letter) => (
+              <button className="key" data-key={letter} key={letter}>
+                {letter}
+              </button>
+            ))}
+            <button data-delete className="key large" onClick={deleteKey}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 0 24 24"
+                width="24"
+              >
+                <path
+                  fill="var(--color-tone-1)"
+                  d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"
+                ></path>
+              </svg>
             </button>
-          ))}
-          <div className="space"></div>
-          <button data-enter className="key large">
-            Enter
-          </button>
-          {"ZXCVBNM".split("").map((letter) => (
-            <button className="key" data-key={letter} key={letter}>
-              {letter}
-            </button>
-          ))}
-          <button data-delete className="key large" onClick={deleteKey}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24"
-              viewBox="0 0 24 24"
-              width="24"
-            >
-              <path
-                fill="var(--color-tone-1)"
-                d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"
-              ></path>
-            </svg>
-          </button>
+          </div>
         </div>
       </main>
     </div>
